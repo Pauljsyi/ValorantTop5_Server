61dfa5c0c768c64d70d418d1885f917ce81929c5
pauljsyi <pauljsyi@gmail.com>
2022-01-23 02:11:34 -0800

    parsed json data from riot

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 const express = require("express");
 const port = process.env.port || 5000;
 const bodyParser = require("body-parser");
+const https = require("https");
 // const path = require("path");
 // const cors = require("cors");
 require("dotenv").config();
 
-let api = process.env.API;
+let api = process.env.RIOT_API;
+let actId = process.env.ACT_ID;
+// let actId1 = process.env.ACT_ID_ONE;
 // console.log(api);
+// console.log(actId);
 
 const app = express();
 // app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 // app.use(express.static(path.join(__dirname, "/../client/dist")));
 // app.use("/", router);
 
-app.get("/", function (req, res) {
-  // const url
+let valorant = app.get("/", function (req, res) {
+  const url = `https://na.api.riotgames.com/val/ranked/v1/leaderboards/by-act/${actId}?size=5&startIndex=0&api_key=${api}`;
+  // const url = "https://valorant-api.com/v1/agents";
+  // console.log(url);
+
+  https.get(url, function (response) {
+    // console.log("RESPONSE", response);
+    response.on("data", function (val) {
+      const valData = JSON.parse(val);
+
+      console.log(valData.players);
+      res.write("Valorant API works!");
+      res.send();
+    });
+  });
 });
 
 app.listen(port, () => {
