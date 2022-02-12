984631ed381648fa2103a8f4a0d6fb733a83b3ca
pauljsyi <pauljsyi@gmail.com>
2022-02-12 04:12:51 -0800

    added trycatch block

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
 const express = require("express");
 const port = process.env.port || 5000;
-const bodyParser = require("body-parser");
-const https = require("https");
 const axios = require("axios");
 require("dotenv").config();
 
-let api = process.env.RIOT_API;
-let actId1 = process.env.ACT_ID_ONE;
+const URL = `https://na.api.riotgames.com/val/ranked/v1/leaderboards/by-act/${process.env.RIOT_API}?size=5&startIndex=0&api_key=${process.env.ACT_ID_ONE}`;
 
 const app = express();
 
-let valorant = app.get("/", async function (req, res) {
-  const url = `https://na.api.riotgames.com/val/ranked/v1/leaderboards/by-act/${actId1}?size=5&startIndex=0&api_key=${api}`;
-
-  // await res.send("Valorant API Working!");
+app.get("/", async function (req, res) {
+  await res.send("Valorant API Working!");
 
   // ====== AXIOS ===== //
 
-  let response = await axios
-    .get(url)
-    .then((res) => {
-      // console.log("working");
-      return res.data;
-    })
-    .catch((e) => {
-      console.log("error msg");
-    });
-  return res.status(200).json(response);
+  try {
+    let response = await axios.get(URL);
+    return res.status(200).json(response.data);
+  } catch (error) {
+    console.log("error: ", error);
+    //error handler
+  }
 });
 
 app.listen(port, () => {
